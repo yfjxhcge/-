b1c183fe323e1d710aba697c16095b7b7fcad60e
yfjxhcge <112472935@qq.com>
2026-08-03 02:34:45 +0800

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -28,7 +28,7 @@ local Toggles = {}
 local Options = {}
 local Tooltips = {}
 
-local BaseURL = "https://raw.githubusercontent.com/deividcomsono/Obsidian/refs/heads/main/"
+local BaseURL = "https://raw.githubusercontent.com/yfjxhcge/-/refs/heads/main/Library.lua"
 local CustomImageManager = {}
 local CustomImageManagerAssets = {
     TransparencyTexture = {
